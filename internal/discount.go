2ae86d8e946789d3417db004c011e367744dd9dc
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-11-26 12:51:44 +0000

    Add discount entity, add product method and test

---
+++ internal/discount.go
@@ -0,0 +1,20 @@
+package internal
+
+import "fmt"
+
+const (
+	BOGOF        string = "Buy One Get One Free"
+	BulkDiscount string = "Bulk Discount"
+)
+
+type Discount struct {
+	ID         int
+	Type       string
+	Percentage int
+	Products   []Product
+}
+
+func (d *Discount) addProduct(product Product) string {
+	d.Products = append(d.Products, product)
+	return fmt.Sprintf("%s added", product.Name)
+}
---
+++ internal/discount_test.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAddProductToDiscount(t *testing.T) {
+	fruitTea := Product{
+		ProductCode: FruitTea,
+		Name:        "Supermarket Fruit Tea",
+		Price:       311,
+	}
+
+	discount := Discount{
+		ID:         1,
+		Type:       BOGOF,
+		Percentage: 100,
+		Products:   []Product{fruitTea},
+	}
+	assert.Equal(t, 1, len(discount.Products))
+
+	strawberries := Product{
+		ProductCode: Strawberries,
+		Name:        "Supermarket Strawberries",
+		Price:       500,
+	}
+
+	message := discount.addProduct(strawberries)
+	assert.Equal(t, 2, len(discount.Products))
+	assert.Equal(t, "Supermarket Strawberries added", message)
+}
