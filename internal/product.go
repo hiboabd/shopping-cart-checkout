52524999de33c3ebac634ab1f0f4afa591ce9bf4
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-11-26 11:07:28 +0000

    Create product entity with product code constants

---
+++ internal/product.go
@@ -0,0 +1,13 @@
+package internal
+
+const (
+	FruitTea     string = "FR1"
+	Strawberries string = "SR1"
+	Coffee       string = "CF1"
+)
+
+type Product struct {
+	ProductCode string
+	Name        string
+	Price       int
+}
